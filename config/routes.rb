aa7e6e5c79dcbff586245f061357e28ffc236861
Muhammed Ahmad <99442871+Muhammed-L-Ahmad@users.noreply.github.com>
2025-09-25 19:30:48 +0000

    added some tentative routes and added a tentative layout erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  # Example route (this is just a sample; you can delete it):
+  get("/home", { :controller => "pages", :action => "home" })
   # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
-  # get("/your_first_screen", { :controller => "pages", :action => "first" })
+  get("/dice/2/6", { :controller => "pages", :action => "2d6" })
+  get("/dice/2/10", { :controller => "pages", :action => "2d10" })
+  get("/dice/1/20", { :controller => "pages", :action => "1d20" })
+  get("/dice/5/4", { :controller => "pages", :action => "5d4" })
 end
