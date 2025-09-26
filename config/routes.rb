49371e9d8ccc751c64bedbb2ef63ab6a4d316d26
Muhammed Ahmad <99442871+Muhammed-L-Ahmad@users.noreply.github.com>
2025-09-26 01:46:21 +0000

    completed controller methods and tried to make them match routes and dice_templates

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # Example route (this is just a sample; you can delete it):
   get("/home", { :controller => "dice", :action => "home" })
   # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
-  get("/dice/2/6", { :controller => "dice", :action => "2d6" })
-  get("/dice/2/10", { :controller => "dice", :action => "2d10" })
-  get("/dice/1/20", { :controller => "dice", :action => "1d20" })
-  get("/dice/5/4", { :controller => "dice", :action => "5d4" })
+  get("/dice/2/6", { :controller => "dice", :action => "two_six" })
+  get("/dice/2/10", { :controller => "dice", :action => "two_ten" })
+  get("/dice/1/20", { :controller => "dice", :action => "one_twenty" })
+  get("/dice/5/4", { :controller => "dice", :action => "five_four" })
 end
