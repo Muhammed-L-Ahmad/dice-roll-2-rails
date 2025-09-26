150ce6b1c14ad952d1046f38be237de9ff172633
Muhammed Ahmad <99442871+Muhammed-L-Ahmad@users.noreply.github.com>
2025-09-26 00:50:57 +0000

    created starter routes, template views, and dice_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # Example route (this is just a sample; you can delete it):
-  get("/home", { :controller => "pages", :action => "home" })
+  get("/home", { :controller => "dice", :action => "home" })
   # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
-  get("/dice/2/6", { :controller => "pages", :action => "2d6" })
-  get("/dice/2/10", { :controller => "pages", :action => "2d10" })
-  get("/dice/1/20", { :controller => "pages", :action => "1d20" })
-  get("/dice/5/4", { :controller => "pages", :action => "5d4" })
+  get("/dice/2/6", { :controller => "dice", :action => "2d6" })
+  get("/dice/2/10", { :controller => "dice", :action => "2d10" })
+  get("/dice/1/20", { :controller => "dice", :action => "1d20" })
+  get("/dice/5/4", { :controller => "dice", :action => "5d4" })
 end
