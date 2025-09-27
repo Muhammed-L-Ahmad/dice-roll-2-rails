caeda37fe80dbacd7138526a2038fd895714de5a
Muhammed Ahmad <99442871+Muhammed-L-Ahmad@users.noreply.github.com>
2025-09-27 02:14:59 +0000

    corrected code for homepage in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Example route (this is just a sample; you can delete it):
-  get("/home", { :controller => "dice", :action => "home" })
+  get("/", { :controller => "dice", :action => "homepage" })
   # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
   get("/dice/2/6", { :controller => "dice", :action => "two_six" })
   get("/dice/2/10", { :controller => "dice", :action => "two_ten" })
