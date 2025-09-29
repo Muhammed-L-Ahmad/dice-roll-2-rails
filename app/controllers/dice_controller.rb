329b6e0d9610241e246d573ff063379d457cb2f0
Muhammed Ahmad <99442871+Muhammed-L-Ahmad@users.noreply.github.com>
2025-09-29 00:18:34 +0000

    Corrected dice_controller to final version per Raghu

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -1,10 +1,24 @@
 class DiceController < ApplicationController
   def two_six
+    @dice_rolls = Array.new
+
+    2.times do
+      new_roll = rand(1..6)
+
+      @dice_rolls.push(new_roll)
+
     render ({ :template => "dice_templates/2d6"})
     
   end
 
   def two_ten
+    @dice_rolls = Array.new
+
+    2.times do
+      new_roll = rand(1..10)
+
+      @dice_rolls.push(new_roll)
+
     render ({ :template => "dice_templates/2d10"})
   end
 
@@ -21,6 +35,13 @@ class DiceController < ApplicationController
   end
 
   def one_twenty
+    @dice_rolls = Array.new
+
+    1.times do
+      new_roll = rand(1..20)
+
+      @dice_rolls.push(new_roll)
+
     render ({ :template => "dice_templates/1d20"})
   end
 
